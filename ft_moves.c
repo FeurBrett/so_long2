0ccd057ef38c79a233fb7b114d371b1610399c9b
Andre Pirovano <apirovan@fu-r7-p4.s19.be>
2023-03-13 14:39:37 +0100

    Ca bouge normalement sauf quand ca passe sur la sortie hihi.

--- ft_moves.c
+++ ft_moves.c
@@ -6,7 +6,7 @@
 /*   By: apirovan <apirovan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 13:08:56 by apirovan          #+#    #+#             */
-/*   Updated: 2023/03/13 13:38:19 by apirovan         ###   ########.fr       */
+/*   Updated: 2023/03/13 14:37:32 by apirovan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,7 +52,7 @@ void	ft_find_p(t_map *map, int i, int j)
 
 void	ft_find_e(t_map *map, int i, int j)
 {
-	printf("find p\n");
+	printf("find e\n");
 	while (map->map[i])
 	{
 		j = 0;
@@ -72,22 +72,19 @@ void	ft_find_e(t_map *map, int i, int j)
 
 void	ft_moves(t_data *data, int keycode)
 {
-	// t_map	*map;
-	// t_img	*img
-
-	// map = data->map;
-	// img = data->img;
 	ft_find_p(data->map, 0, 0);
-	if (keycode == W && data->map->map[(data->map->p[1]) - 1][data->map->p[0]] != '1')
+	if (keycode == W &&
+		data->map->map[(data->map->p[1]) - 1][data->map->p[0]] != '1')
 		move_up(data, data->map->p[0], data->map->p[1]);
-	if (keycode == S && data->map->map[(data->map->p[1]) + 1][data->map->p[0]] != '1')
+	if (keycode == S &&
+		data->map->map[(data->map->p[1]) + 1][data->map->p[0]] != '1')
 		move_down(data, data->map->p[0], data->map->p[1]);
-	if (keycode == D && data->map->map[data->map->p[1]][data->map->p[0] + 1] != '1')
+	if (keycode == D &&
+		data->map->map[data->map->p[1]][data->map->p[0] + 1] != '1')
 		move_right(data, data->map->p[0], data->map->p[1]);
-	if (keycode == A && data->map->map[data->map->p[1]][data->map->p[0] - 1] != '1')
+	if (keycode == A &&
+		data->map->map[data->map->p[1]][data->map->p[0] - 1] != '1')
 		move_left(data, data->map->p[0], data->map->p[1]);
-	// if (ft_collected(map) == 1 && map->map[map->p[0]][map->p[1]] == 'E')
-	// 	ft_win(map, img);
 	printf("p after move is in %d, %d\n", data->map->p[1], data->map->p[0]);
 }
 
